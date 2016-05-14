79731dd0705613ed4fe84a15f3ff90a7fa4d9d41
Kevin Ispiroglu Lindblad <freiyan@gmail.com>
2016-05-14 17:17:06 +0200

    Started fixing up the AJAX request

--- weatherApp/weather.js
+++ weatherApp/weather.js
@@ -5,6 +5,8 @@
 
 var lat = "";
 var long = "";
+var temp  = "";
+var city = "";
 //Get latitude and longitude;
 function successFunction(position) {
     lat = position.coords.latitude;
@@ -14,6 +16,19 @@ function successFunction(position) {
 function errorFunction() {
 	alert('WARNING! your browser appear to not be supporting geolocation');
 } 
+
+function getWeatherByLocation() {
+	$.ajax({
+		url: 'http://api.openweathermap.org/data/2.5/weather?lat='+lat+'&lon='+long+'&appid=4d89068a0a59e956b2aab4c74b849776',
+		type: 'GET',
+		data: {}, 
+		dataType: 'JSON',
+		success: function(data) {
+			
+		}
+	})
+}
+
 
 $(document).ready(function() {
 	//Check if browser supports W3C Geolocation API
