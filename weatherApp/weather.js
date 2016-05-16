cf34feb8926fe85e3005bcc88726b559911d3fe4
Kevin Ispiroglu Lindblad <freiyan@gmail.com>
2016-05-17 00:13:51 +0200

    Changed structure of the code as to gain access to some of the variables.

--- weatherApp/weather.js
+++ weatherApp/weather.js
@@ -9,15 +9,16 @@ var country = '';
 var weatherAPI = '';
 
 $(document).ready(function() {
-	getLocation();
+	getLocationWeather();
 
 	//Switching location API's now trying to use IPLOCATION
-	function getLocation() {
+	function getLocationWeather() {
 	   	 $.getJSON('http://ipinfo.io', function(data){
 	 		 console.log(data);
 	 		 city = data.city+','+data.country;
 	 		 country = data.country;
 	 		 weatherAPI += 'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=4d89068a0a59e956b2aab4c74b849776';
+	 		 // Making the API call here, because otherwise there is no access to the newly changed weatherAPI var.
 	 		 getWeatherByLocation(weatherAPI);
 		});
 	}
