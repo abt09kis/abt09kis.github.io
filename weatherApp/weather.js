7ca5592d03b91c7d2cdbee2ac64e5cea4134cb20
Kevin Ispiroglu Lindblad <freiyan@gmail.com>
2016-05-16 20:52:48 +0200

    Trying to use getJSON() instead of using ajax()

--- weatherApp/weather.js
+++ weatherApp/weather.js
@@ -19,12 +19,13 @@ function errorFunction() {
 } 
 var weatherAPI ="";
 function getRequestURL() {
-	weatherAPI = 'http://api.openweathermap.org/data/2.5/weather?lat='+lat+'&lon='+long+'&appid=4d89068a0a59e956b2aab4c74b849776';
+	weatherAPI += "http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + long + "&appid=4d89068a0a59e956b2aab4c74b849776";
 }
 
 function getWeatherByLocation() {
+	/*
 	if(lat !=="" && long !==""){
-		/*
+		
 		$.ajax({
 			url: 'http://api.openweathermap.org/data/2.5/weather?lat='+lat+'&lon='+long+'&appid=4d89068a0a59e956b2aab4c74b849776',
 			type: 'GET',
@@ -43,13 +44,14 @@ function getWeatherByLocation() {
 				xhr.setRequestHeader("openweathermap", "4d89068a0a59e956b2aab4c74b849776");
 			}
 		});
+		}
 		*/
 		$.getJSON(weatherAPI, function(wd){
 			temp = wd.main.temp;
 			country = wd.sys.country;
 			city = wd.name;
 		});
-	}	
+		
 }
 
 
