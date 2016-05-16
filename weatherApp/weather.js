8c97d3f76e3977438316c0f0574f37d1f71d44a0
Kevin Ispiroglu Lindblad <freiyan@gmail.com>
2016-05-16 21:38:21 +0200

    Trying to use getJSON() instead of using ajax()

--- weatherApp/weather.js
+++ weatherApp/weather.js
@@ -6,16 +6,7 @@
 var temp  = "";
 var city = "";
 var country = "";
-//Get latitude and longitude;
-/*
-function successFunction(position) {
-    lat = position.coords.latitude;
-    long = position.coords.longitude;
-}
 
-function errorFunction() {
-	alert('WARNING! your browser appear to not be supporting geolocation');
-} */
 //Switching location API's now trying to use IPLOCATION
 function getLocation() {
    	 $.getJSON('http://ipinfo.io', function(data){
@@ -27,40 +18,14 @@ function getLocation() {
 
 var weatherAPI = "";
 function getRequestURL() {
-	var tmp = city+","+country;
+	var tmp = city+","+country;	
 	weatherAPI += "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=4d89068a0a59e956b2aab4c74b849776";
 }
 
 function getWeatherByLocation() {
-	/*
-	if(lat !=="" && long !==""){
-		
-		$.ajax({
-			url: 'http://api.openweathermap.org/data/2.5/weather?lat='+lat+'&lon='+long+'&appid=4d89068a0a59e956b2aab4c74b849776',
-			type: 'GET',
-			data: {}, 
-			dataType: 'JSON',
-			success: function(data) {
-				temp = data.main.temp;
-				city = data.name;
-				country = data.sys.country;
-			},
-			error: function(err) {
-				alert(err);
-			},
-			
-			beforeSend: function(xhr) {
-				xhr.setRequestHeader("openweathermap", "4d89068a0a59e956b2aab4c74b849776");
-			}
-		});
-		}
-		
 		$.getJSON(weatherAPI, function(wd){
 			temp = wd.main.temp;
-			country = wd.sys.country;
-			city = wd.name;
 		});
-		*/
 }
 
 
