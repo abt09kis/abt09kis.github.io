9fa0a1fca755d8c9e415d67b7081ff44ec98d6a6
Kevin Ispiroglu Lindblad <freiyan@gmail.com>
2016-05-16 22:37:23 +0200

    Trying to use getJSON() instead of using ajax()

--- weatherApp/weather.js
+++ weatherApp/weather.js
@@ -3,27 +3,27 @@
 
 */
 
-var temp  = "";
-var city = "";
-var country = "";
-var weatherAPI = "";
+var temp  = '';
+var city = '';
+var country = '';
+var weatherAPI = '';
 
 $(document).ready(function() {
 	getLocation();
-	getWeatherByLocation();
+	getWeatherByLocation(weatherAPI);
 
 	//Switching location API's now trying to use IPLOCATION
 	function getLocation() {
 	   	 $.getJSON('http://ipinfo.io', function(data){
 	 		 console.log(data);
-	 		 city = data.city+","+data.country;
+	 		 city = data.city+','+data.country;
 	 		 country = data.country;
-	 		 weatherAPI += "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=4d89068a0a59e956b2aab4c74b849776";
+	 		 weatherAPI += 'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=4d89068a0a59e956b2aab4c74b849776';
 		});
 	}
 
-	function getWeatherByLocation() {
-			$.getJSON(weatherAPI, function(wd){
+	function getWeatherByLocation(url) {
+			$.getJSON(url, function(wd){
 				console.log(wd);
 				temp = wd.main.temp;
 			});
