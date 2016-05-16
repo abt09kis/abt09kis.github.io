24d5556965ab6129f5b31817f66fe7289b506cbe
Kevin Ispiroglu Lindblad <freiyan@gmail.com>
2016-05-16 22:29:35 +0200

    Trying to use getJSON() instead of using ajax()

--- weatherApp/weather.js
+++ weatherApp/weather.js
@@ -8,30 +8,32 @@ var city = "";
 var country = "";
 var weatherAPI = "";
 
-//Switching location API's now trying to use IPLOCATION
-function getLocation() {
-   	 $.getJSON('http://ipinfo.io', function(data){
- 		 console.log(data);
- 		 city = data.city+","+data.country;
- 		 country = data.country;
- 		 weatherAPI += "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=4d89068a0a59e956b2aab4c74b849776";
-	});
-}
-
-function getWeatherByLocation() {
-		$.getJSON(weatherAPI, function(wd){
-			temp = wd.main.temp;
-		});
-}
-
-
 $(document).ready(function() {
+	getLocation();
+	getWeatherByLocation();
+
+	//Switching location API's now trying to use IPLOCATION
+	function getLocation() {
+	   	 $.getJSON('http://ipinfo.io', function(data){
+	 		 console.log(data);
+	 		 city = data.city+","+data.country;
+	 		 country = data.country;
+	 		 weatherAPI += "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=4d89068a0a59e956b2aab4c74b849776";
+		});
+	}
+
+	function getWeatherByLocation() {
+			$.getJSON(weatherAPI, function(wd){
+				temp = wd.main.temp;
+			});
+	}
+
+
+
 	/*
 	//Check if browser supports W3C Geolocation API
 	if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(successFunction, errorFunction);
 	} 
 	*/
-	getLocation();
-	//getWeatherByLocation();
 });
