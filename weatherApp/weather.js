762f878f7ebb698d4387e98e0cffdcdc9e0a8b1d
Kevin Ispiroglu Lindblad <freiyan@gmail.com>
2016-10-20 07:58:06 +0200

    started using promises

--- weatherApp/weather.js
+++ weatherApp/weather.js
@@ -13,15 +13,28 @@ $(document).ready(function() {
 
 	//Using IPLOCATION API to get access to location data in order to retrieve weather info.
 	function getLocationWeather() {
-	   	 $.getJSON('http://ipinfo.io', function(data){
-	 		 console.log(data);
-	 		 city = data.city+','+data.country;
-	 		 country = data.country;
-	 		 // In case the city data is empty, only country is passed on to the API call, resulting in capital!
-	 		 weatherAPI += 'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=4d89068a0a59e956b2aab4c74b849776';
-	 		 // Making the API call here, because otherwise there is no access to the newly changed weatherAPI var.
-	 		 getWeatherByLocation(weatherAPI);
-		});
+		var p1 = new Promise(
+			function(resolve,reject) {
+		   	$.getJSON('http://ipinfo.io', function(data){
+			 		console.log(data);
+			 		city = data.city+','+data.country;
+			 		country = data.country;
+			 		// In case the city data is empty, only country is passed on to the API call, resulting in capital!
+			 		weatherAPI += 'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=4d89068a0a59e956b2aab4c74b849776';
+			 		// Making the API call here, because otherwise there is no access to the newly changed weatherAPI var.
+			 		resolve(weatherAPI);
+			 		//getWeatherByLocation(weatherAPI);
+	 			});
+			});
+		p1.then(
+				function(val) {
+					getWeatherByLocation(val);
+				})
+		.catch(
+				function(reason) {
+					console.log('Handle the rejected promies (' +reason +' ) here');
+				}
+			)
 	}
 
 	function getWeatherByLocation(url) {
