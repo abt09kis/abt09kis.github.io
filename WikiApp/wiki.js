0b3456b13cff12d3b800e9b64c03dba146a658f4
Kevin Ispiroglu Lindblad <freiyan@gmail.com>
2016-11-26 10:35:27 +0100

    updated files

--- WikiApp/wiki.js
+++ WikiApp/wiki.js
@@ -1,8 +1,23 @@
+Vue.component('search-wiki',{
+	template: '#searchWiki',
+	data() {
+		return {
+			title: "",
+			searching: false
+		}
+	},
+	methods: {
+		onSearch: function() {
+			var BASE_URL = "https://en.wikipedia.org/w/api.php?action=query&titles=";
+			var endUrl = "&prop=revisions&rvprop=content&format=json";
 
+			this.searching = true;
+			console.log(BASE_URL + this.title + endUrl);
+		}
+	}
+})
 
 var app = new Vue({
   el: '#app',
-  data: {
-  	searchstring: "Hej"
-  }
+  template: `<search-wiki></search-wiki>`
 })
